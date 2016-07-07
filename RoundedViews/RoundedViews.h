98564082cbd6eef6c8a212c113d6f0977ce62310
Juan Alberto Uribe <juanalberto@kogimobile.com>
2016-07-07 10:10:26 -0500

    Initial Commit

---
+++ RoundedViews/RoundedViews.h
@@ -0,0 +1,19 @@
+//
+//  RoundedViews.h
+//  RoundedViews
+//
+//  Created by Juan Alberto Uribe on 7/7/16.
+//  Copyright © 2016 Kogi Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RoundedViews.
+FOUNDATION_EXPORT double RoundedViewsVersionNumber;
+
+//! Project version string for RoundedViews.
+FOUNDATION_EXPORT const unsigned char RoundedViewsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RoundedViews/PublicHeader.h>
+
+
