50ff31b282aff4a792ee6c895493a72b01f8f262
Juan Alberto Uribe <juanalberto@kogimobile.com>
2016-07-07 11:59:45 -0500

    Add rounded image

---
+++ Source/RoundedImage.swift
@@ -0,0 +1,32 @@
+//
+//  RoundedImage.swift
+//  RoundedViews
+//
+//  Created by Juan Alberto Uribe on 7/7/16.
+//  Copyright © 2016 Kogi Mobile. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable public class RoundedImage: UIImageView, Rounded {
+    @IBInspectable public var radiusMultiplier: CGFloat = 1 {
+        didSet {
+            updateCorderRadius()
+        }
+    }
+    @IBInspectable public var borderWidth: CGFloat = 0 {
+        didSet {
+            updateBorderWidth()
+        }
+    }
+    @IBInspectable public var borderColor: UIColor? {
+        didSet {
+            updateBoderColor()
+        }
+    }
+    override public var bounds: CGRect {
+        didSet {
+            updateCorderRadius()
+        }
+    }
+}
